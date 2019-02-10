42ef3df0d3c90eb483546563646d878647cb0816
sleeplesseditor <josh@bryson.me.uk>
2019-02-10 11:34:01 +0000

    Styling updated and PDF creation routes created

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const pdf = require('html-pdf');
-const cors = request('cors');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 5000;
+const pdfTemplate = require('./documents');
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,3 +14,16 @@ app.use(bodyParser.json());
 app.listen(port, () => console.log(
     `Listening on Port ${port}`
 ));
+
+app.post('/create-pdf', (req, res) => {
+    pdf.create(pdfTemplate(req.body), {}).toFile('result.pdf', (err) => {
+        if(err) {
+            return console.log('Error', err);
+        }
+        res.send(Promise.resolve())
+    });
+});
+
+app.get('/fetch-pdf', (req, res) => {
+    res.sendFile(`${__dirname}/result.pdf`);
+});
